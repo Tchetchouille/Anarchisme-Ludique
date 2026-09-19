819e69bceb823e6a0379b4186ae53d1363b26f48
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2026-09-19 21:33:04 +0200

    Polish pendaison

--- le_proprietaire/hang_scenes/hangperson.gd
+++ le_proprietaire/hang_scenes/hangperson.gd
@@ -3,11 +3,12 @@ extends Node2D
 var current_step : int = 0
 # Will hold the number of steps for drawing the scaffold
 var scaffold_threshold : int
-
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	scaffold_threshold = $Scaffold.get_child_count()
+	pends_ton_proprietaire()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -27,3 +28,7 @@ func advance(step):
 
 func activate_bodypart(i):
 	pass
+	
+func pends_ton_proprietaire():
+	var random_impulse = rng.randi_range(-400.0, 400.0)
+	$Body/Body.apply_impulse(Vector2(random_impulse, 0))
